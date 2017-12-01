4615722dc74960ce39e875ba56a7c12d930afaa1
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-12-01 15:34:13 -0500

    Finished simple program.

---
+++ App.cs
@@ -0,0 +1,121 @@
+using System;
+
+//Namespace
+namespace NumberGuesser
+{
+    //Main Class 
+    class Program
+    {
+        //Entry Point Method 
+        static void Main(string[] args)
+        {
+
+            //run function to get app info 
+            GetAppInfo();
+
+            GreetUser(); //Ask for users name and greet
+
+            while (true)
+            {
+                //Set correct number
+                //int correctNumber = 7;
+
+                //Create random number 
+
+                Random random = new Random();
+
+                int correctNumber = random.Next(1, 10);
+
+                //Init guess var
+                int guess = 0;
+
+                Console.WriteLine("Guess a number between 1 and 10");
+
+                while (guess != correctNumber)
+                {
+                    //Get user input
+                    string input = Console.ReadLine();
+
+
+                    //Make sure its a number 
+
+                    if (!int.TryParse(input, out guess))
+                    {
+
+                        PrintColorMessage(ConsoleColor.DarkRed, "Please enter an actual number");
+                        //keep going 
+                        continue;
+
+
+                    }
+                    //Cast to int and put in guess
+                    guess = Int32.Parse(input);
+
+                    //match correct number 
+
+                    if (guess != correctNumber)
+                    {
+                        PrintColorMessage(ConsoleColor.Red, "Wrong Number Please Guess again");
+                    }
+
+                }
+
+                //Output Success message
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.WriteLine("You guessed the correct number!");
+
+                Console.ResetColor();
+
+                Console.WriteLine("Do you want to play again? [Y or N]");
+
+                string answer = Console.ReadLine().ToUpper();
+
+                if (answer == "Y")
+                {
+                    continue;
+                }else if (answer =="N"){
+                        
+                    return;
+                }else{
+                    return;
+                }
+            }
+
+        }
+
+        static void GetAppInfo(){
+
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Jason Navarro";
+
+            //change text color 
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            Console.ResetColor();
+        }
+
+        static void GreetUser(){
+            //Ask users name
+            Console.WriteLine("What is your name?");
+
+            // Get user input
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, Let's play a game...", inputName);
+
+        }
+
+        static void PrintColorMessage(ConsoleColor color, string message){
+
+            Console.ForegroundColor =  color;
+
+            Console.WriteLine(message);
+
+            Console.ResetColor();
+
+        }
+    }
+}
